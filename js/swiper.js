a2211a4d0c238de63792512ca17d03561a5bf7b5
Evgeniy Nekipelov <johnnykayfun@gmail.com>
2025-07-14 00:40:32 +0300

    add slider, breadcrumbs

---
+++ js/swiper.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const swiper = new Swiper(".swiper", {
+    loop: true,
+    autoplay: {
+      delay: 5000,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+});
