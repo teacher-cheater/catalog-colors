ebbd08e97cdb6c315cd38d7884228b4386334412
Evgeniy Nekipelov <johnnykayfun@gmail.com>
2025-07-15 18:57:28 +0300

    fix styles

--- js/burger.js
+++ js/burger.js
@@ -1,10 +1,10 @@
-const burger = document.querySelector('.header__burger');
-const menu = document.querySelector('.header__list');
-const overlay = document.querySelector('.header__overlay');
+const burger = document.querySelector(".header__burger");
+const menu = document.querySelector(".header__list");
+const overlay = document.querySelector(".header__overlay");
 
-burger.addEventListener('click', () => {
-  burger.classList.toggle('_open-burger');
-  menu.classList.toggle('_open-burger');
-  overlay.classList.toggle('_open-burger');
-  document.body.classList.toggle('_lock'); 
+burger.addEventListener("click", () => {
+  burger.classList.toggle("_open-burger");
+  menu.classList.toggle("_open-burger");
+  overlay.classList.toggle("_open-burger");
+  document.body.classList.toggle("no-scroll");
 });
